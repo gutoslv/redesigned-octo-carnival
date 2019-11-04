39474d0bff087a13cf461846548841598254713d
Augusto Cesar da Silva <guto_slv@live.com>
2019-11-03 22:03:18 -0300

    Adiciona teardown no RunCucumberTest
    Encerra todas as sessões do Driver ao terminar os testes
    
    Signed-off-by: Augusto Cesar da Silva <guto_slv@live.com>

--- src/test/java/DesafioZup/RunCucumberTest.java
+++ src/test/java/DesafioZup/RunCucumberTest.java
@@ -2,9 +2,15 @@ package DesafioZup;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
+import pageObject.DriverFactory;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = {"pretty"})
 public class RunCucumberTest {
+  @AfterClass
+  public static void teardown(){
+    DriverFactory.closeDriver();
+  }
 }
